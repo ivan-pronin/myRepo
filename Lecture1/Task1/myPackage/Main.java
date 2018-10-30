797681c6f1f9f6e504020bbedc128a6208ecc4d2
Ivan Pronin <ivanp@playtika.com>
2018-10-30 17:10:33 +0300

    Add lecture 1 task 1 implementation"

---
+++ Lecture1/Task1/myPackage/Main.java
@@ -0,0 +1,11 @@
+package myPackage;
+
+/**
+ * Created by ivanp on 30-Oct-18
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
